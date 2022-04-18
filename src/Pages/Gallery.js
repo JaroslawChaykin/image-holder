18fe96eb4e357b01ffa5e80e5f743e646c1ac1a6
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-04-18 21:21:54 +0300

    created hash for tab navigate

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import ImageList from '../Components/ImageList';
-import { Accordion, Container, Row, Tab, Tabs } from 'react-bootstrap';
+import { Container, Spinner, Tab, Tabs } from 'react-bootstrap';
 
 const Gallery = () => {
 
+    const getNumOfHash = (str) => {
+        if (str.includes('activetab')) {
+            return str.substring(window.location.hash.length - 1)
+        }
+        return str
+    }
+
     const [images, setImages] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [tabHash, setTabHash] = useState(getNumOfHash(window.location.hash));
+
+    window.location.hash = `activetab=${tabHash || 0}`
 
     const fetchImages = async () => {
         try {
@@ -25,12 +35,20 @@ const Gallery = () => {
       <Container>
           {
               isLoading
-                ? <h1>Загрузка...</h1>
+                ?
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+                    <Spinner animation="grow" variant="primary"/>
+                </div>
                 :
-                <Tabs defaultActiveKey="0" id="uncontrolled-tab-example" className="mb-3">
+                <Tabs defaultActiveKey={tabHash}
+                      id="uncontrolled-tab-example"
+                      className="mt-5"
+                      onClick={(e) => e.target.type === 'button' && setTabHash(e.target.dataset.rrUiEventKey)}>
                     {
                         images.map((item, index) =>
-                          <Tab eventKey={index} title={`Группа ${index + 1}`}>
+                          <Tab key={index}
+                               eventKey={index}
+                               title={`Группа ${index + 1}`}>
                               <ImageList images={images[index]}/>
                           </Tab>
                         )
